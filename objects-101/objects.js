b016c19f3bbc35b91c0618ad55dabfd62a394074
cmobley001 <cmobley001@student.butte.edu>
2025-10-18 13:54:10 -0700

    JS Objects lab

---
+++ objects-101/objects.js
@@ -0,0 +1,20 @@
+// 1.0 Use console.log to output your name
+console.log("My name is Chris Mobley");
+
+// 1.1 Use object longhand to create an object with properties: firstName, lastName
+
+// 1.2 Use JSON.stringify to console.log the object
+
+// 1.3 Use "dot notation" to console.log the firstName property
+
+// 1.4 Use "square brackets" to console.log the lastName property
+
+// 1.5 Use object shorthand to create an object with properties: title, subtitle. console.log the object
+
+// 1.6 Use destructuring to pull the firstName property out of an object. console.log the firstName
+
+// 1.7 Use destructured renaming to pull the lastName property out of an object, rename the property to "surname". console.log the surname.
+
+// 1.8 Use spread syntax to create a new object from the old object, but also adds a new property called "secret". console.log the secret
+
+// 1.9 Use rest syntax to pull out the "secret" property, but put everything else into the personWithoutSecret property. console.log the personWithoutSecret
